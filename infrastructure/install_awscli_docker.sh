9c4b2b5c350a1b3e3a443330fb9bacd701c41e48
iu <ilya.romanovskiy.2015@mail.ru>
2023-03-10 12:58:19 +0300

    ads

--- infrastructure/install_awscli_docker.sh
+++ infrastructure/install_awscli_docker.sh
@@ -3,13 +3,15 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 rm get-docker.sh
 
+touch a1
+
 sudo usermod -aG docker ubuntu
 
 
 sudo apt install --yes  python3-pip
 sudo pip3 install docker-compose
 
-
+touch a2
 
 git clone https://github.com/midi-chlorians8/ToDo_Web_app3.git
 cd ToDo_Web_app3/
